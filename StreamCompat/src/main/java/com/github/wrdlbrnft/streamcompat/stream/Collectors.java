8bb1d2c05f8c01e303726c04dd2370e40ff6be3a
Xaver Kapeller <xaver.kapeller@gmail.com>
2016-03-22 15:50:25 +0100

    Added grouping Collectors.

--- StreamCompat/src/main/java/com/github/wrdlbrnft/streamcompat/stream/Collectors.java
+++ StreamCompat/src/main/java/com/github/wrdlbrnft/streamcompat/stream/Collectors.java
@@ -10,7 +10,9 @@ import com.github.wrdlbrnft.streamcompat.function.Function;
 import com.github.wrdlbrnft.streamcompat.function.Supplier;
 import com.github.wrdlbrnft.streamcompat.function.ToIntFunction;
 import com.github.wrdlbrnft.streamcompat.function.ToLongFunction;
+import com.github.wrdlbrnft.streamcompat.util.MapCompat;
 import com.github.wrdlbrnft.streamcompat.util.StringJoiner;
+import com.github.wrdlbrnft.streamcompat.util.Utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -118,7 +120,7 @@ public class Collectors {
             final int key = keyMapper.apply(element);
             final U current = map.get(key);
             final U value = current != null
-                    ? mergeFunction.apply(current,  valueMapper.apply(element))
+                    ? mergeFunction.apply(current, valueMapper.apply(element))
                     : valueMapper.apply(element);
             map.put(key, value);
         };
@@ -137,7 +139,7 @@ public class Collectors {
             final long key = keyMapper.apply(element);
             final U current = map.get(key);
             final U value = current != null
-                    ? mergeFunction.apply(current,  valueMapper.apply(element))
+                    ? mergeFunction.apply(current, valueMapper.apply(element))
                     : valueMapper.apply(element);
             map.put(key, value);
         };
@@ -145,7 +147,7 @@ public class Collectors {
     }
 
     public static <T, U> Collector<T, ?, LongSparseArray<U>> toLongSparseArray(ToLongFunction<? super T> keyMapper,
-                                                                       Function<? super T, ? extends U> valueMapper) {
+                                                                               Function<? super T, ? extends U> valueMapper) {
         return toLongSparseArray(keyMapper, valueMapper, throwingMerger());
     }
 
@@ -153,6 +155,41 @@ public class Collectors {
         return (t, u) -> {
             throw new IllegalStateException("Multiple values mapped to the same key");
         };
+    }
+
+    public static <T, K, D, A, M extends Map<K, D>> Collector<T, ?, M> groupingBy(
+            Function<? super T, ? extends K> classifier,
+            Supplier<M> mapFactory,
+            Collector<? super T, A, D> downstream) {
+
+        final Supplier<A> downstreamSupplier = downstream.supplier();
+        final BiConsumer<A, ? super T> downstreamAccumulator = downstream.accumulator();
+        final BiConsumer<Map<K, A>, T> accumulator = (m, t) -> {
+            K key = Utils.requireNonNull(classifier.apply(t), "element cannot be mapped to a null key");
+            A container = MapCompat.computeIfAbsent(m, key, k -> downstreamSupplier.get());
+            downstreamAccumulator.accept(container, t);
+        };
+
+        @SuppressWarnings("unchecked")
+        final Supplier<Map<K, A>> mangledFactory = (Supplier<Map<K, A>>) mapFactory;
+
+        @SuppressWarnings("unchecked")
+        final Function<A, A> downstreamFinisher = (Function<A, A>) downstream.finisher();
+        final Function<Map<K, A>, M> finisher = intermediate -> {
+            MapCompat.replaceAll(intermediate, (k, v) -> downstreamFinisher.apply(v));
+
+            //noinspection unchecked
+            return (M) intermediate;
+        };
+        return new CollectorImpl<>(mangledFactory, accumulator, finisher);
+    }
+
+    public static <T, K, A, D> Collector<T, ?, Map<K, D>> groupingBy(Function<? super T, ? extends K> classifier, Collector<? super T, A, D> downstream) {
+        return groupingBy(classifier, ArrayMap::new, downstream);
+    }
+
+    public static <T, K> Collector<T, ?, Map<K, List<T>>> groupingBy(Function<? super T, ? extends K> classifier) {
+        return groupingBy(classifier, toList());
     }
 
     public static <T, A, R> Collector<T, A, R> create(Supplier<A> supplier, BiConsumer<A, T> accumulator, Function<A, R> finisher) {
